5443648b2e30f8662706adf01d951a49394488ba
wesson <vinokurov.filipp1@yandex.ru>
2023-06-23 12:49:47 +0300

    55, Jump Game

--- leetcode/55. Jump Game.py
+++ leetcode/55. Jump Game.py
@@ -4,10 +4,11 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         last_index = len(nums) - 1
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= last_index:
                 last_index = i
-        if last_index == 0:
-            return True
+        return not last_index
 
 
+# Runtime 450 ms, beats 90.72%
+# Memory 17.5 MB beats 84.5%
