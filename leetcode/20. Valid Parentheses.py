495cf5a1b4d981e4d29ce5ca723308c3c8967967
wesson <vinokurov.filipp1@yandex.ru>
2023-07-07 11:02:23 +0300

    .

--- leetcode/20. Valid Parentheses.py
+++ leetcode/20. Valid Parentheses.py
@@ -1,18 +1,15 @@
 class Solution:
     def isValid(self, s):
         stack = []
-        for c in s:
-            if c in ['(', '{', '[']:
-                stack.append(c)
-            elif c == ')' and len(stack) != 0 and stack[-1] == '(':
-                stack.pop()
-            elif c == '}' and len(stack) != 0 and stack[-1] == '{':
-                stack.pop()
-            elif c == ']' and len(stack) != 0 and stack[-1] == '[':
-                stack.pop()
-            else:
+        parenthesis = {"(": ")", "{": "}", "[": "]"}
+
+        for char in s:
+            if char in parenthesis:
+                stack.append(char)
+            elif not stack or parenthesis[stack.pop()] != char:
                 return False
-        return stack == []
+
+        return not stack
 
 
 print(Solution().isValid("(([]){})"))
